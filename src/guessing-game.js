0d79274c1f00d8ad3ac1c0de2f8e2e16afc9fce3
Aleksey Pashchuk <aleksey.pashchuk@gmail.com>
2023-02-23 00:07:40 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.reasult = Math.round((this.max - this.min) / 2) + this.min;
+        return this.reasult;
     }
 
     lower() {
-
+        this.max = this.reasult;
     }
 
     greater() {
-
+        this.min = this.reasult;
     }
 }
 
